2e81f174cd64a8459d9f60f6c8914d0bb9afa17f
Cicero Gomes <cicero.ufsc@gmail.com>
2024-11-01 11:17:40 -0300

    Organiza todas as entidades e cria a classe entity como base das entidades

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto"
+import { UniqueEntityID } from './unique-entity-id'
 
 export class Entity<Props> {
-    private _id: string
-    protected props: Props
-    
-    get id() {
-        return this._id
-    }
+  private _id: UniqueEntityID
+  protected props: Props
 
-    constructor(props: Props, id?: string) {
-        this.props = props
-        this._id = id ?? randomUUID()
-    }
+  get id() {
+    return this._id
+  }
+
+  protected constructor(props: Props, id?: UniqueEntityID) {
+    this.props = props
+    this._id = id ?? new UniqueEntityID()
+  }
 }
--- src/domain/use-cases/awnser-question.spec.ts
+++ src/domain/use-cases/awnser-question.spec.ts
@@ -1,22 +1,20 @@
-import { expect, test } from 'vitest'
 import { Answer } from '../entities/answer'
-import { AnswersRepository } from '../repositories/answers-repository';
-import { AnswerQuestionUseCase } from './awnser-question';
+import { AnswersRepository } from '../repositories/answers-repository'
+import { AnswerQuestionUseCase } from './awnser-question'
 
 const fakeAwnsersRepository: AnswersRepository = {
-    create: async (answer: Answer) => {
-        return;
-    }
+  create: async (answer: Answer) => {
+  },
 }
 
 test('create an anwser', async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAwnsersRepository)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAwnsersRepository)
 
-    const answer = await answerQuestion.execute({
-        questionId: '1',
-        instructorId: '1',
-        content: 'Nova resposta'
-    })
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
 
-    expect(answer.content).toEqual('Nova resposta')
+  expect(answer.content).toEqual('Nova resposta')
 })
